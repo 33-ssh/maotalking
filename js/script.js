a6d8de547e6210d4880c5e6c4d576256f21a4b5f
33-ssh <mountainxu33@gmail.com>
2024-09-18 10:35:09 +0800

    gotop

--- js/script.js
+++ js/script.js
@@ -29,3 +29,18 @@ window.addEventListener('scroll', () => {
         }
     }
 });
+
+// 滑動至頂
+
+$('#gotop').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 1000);
+});
+
+// 至頂按鈕淡出淡入
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('#gotop').stop().fadeTo('fast', 1);
+    } else {
+        $('#gotop').stop().fadeOut('fast');
+    }
+});
